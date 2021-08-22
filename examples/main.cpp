5877874f5ddff925be3f8419cbf5f65db83c4cdc
Maxime Chéramy <maxime@cheramy.org>
2021-08-22 11:26:10 +0200

    Add comments

--- examples/main.cpp
+++ examples/main.cpp
@@ -41,16 +41,20 @@ void setupButtons()
 {
   buttonManager.setup();
 
-  playlistButton = buttonManager.addButton(80, 120, handler);
-  playlist2Button = buttonManager.addButton(160, 200, handler);
+  // Pin is connected to ground through a 100kOhm resistor (R0).
+  // Each button is connected to the pin and the 5V through a resistor (R1) of different values.
+  // The low and high value are close to: 1024 * (1 - R1 / (R0 + R1))
 
+  playlistButton = buttonManager.addButton(80, 120, handler); // 1MOhm
+  playlist2Button = buttonManager.addButton(160, 200, handler); // 470kOhm
+  nextSongButton = buttonManager.addButton(290, 350, handler); // 220kOhm
+  prevSongButton = buttonManager.addButton(480, 540, handler); // 100kOhm
+  volumeDownButton = buttonManager.addButton(620, 750, handler); // 47kOhm
+  volumeUpButton = buttonManager.addButton(940, 1024, handler); // 1kOhm
+  
+  // It's also possible to detect when 2 buttons are pressed simultaneously since it creates a new resistance value
   //playlist1and2Buttons = buttonManager.addButton(230, 260, handler);
-
-  nextSongButton = buttonManager.addButton(290, 350, handler);
-  prevSongButton = buttonManager.addButton(480, 540, handler);
   //nextAndPrevButtons = buttonManager.addButton(570, 610, handler);
-  volumeDownButton = buttonManager.addButton(620, 750, handler);
-  volumeUpButton = buttonManager.addButton(940, 1024, handler);
 }
 
 void setup()
